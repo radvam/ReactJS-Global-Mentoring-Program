6aa45eaa530463c936777fa44cc7c3ad8ae45ec7
Dmitriy_Radcovetc <dmitriy_radcovetc@epam.com>
2021-04-12 16:19:57 +0300

    feat: replace Formik to useFormik hook

--- my-app/src/components/common/Form/form-movie/FormMovie.tsx
+++ my-app/src/components/common/Form/form-movie/FormMovie.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from "react";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 
 import { genresData, validationSchema } from "./utils/FormMovieUtils";
 
@@ -33,125 +33,119 @@ export const FormMovie: FC<FormMovieProps> = ({
   resetButtonText,
   submitButtonText,
   id,
-}): React.ReactElement => (
-  <Formik
-    initialValues={formInitialValues}
-    validationSchema={validationSchema}
-    onSubmit={async (values) => {
+}): React.ReactElement => {
+  const formik = useFormik({
+    initialValues: formInitialValues,
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
       setOpenModal(false);
-      await onSubmit(values);
-    }}
-  >
-    {({
-      values,
-      errors,
-      touched,
-      handleChange,
-      handleReset,
-      handleBlur,
-      handleSubmit,
-      isSubmitting,
-    }) => (
-      <form onSubmit={handleSubmit}>
-        <FormMovieWrapper>
-          {id && (
-            <FormItem>
-              <Title>{ID_LABEL}</Title>
-              <MovieId>{id}</MovieId>
-            </FormItem>
+      onSubmit(values);
+    },
+  });
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <FormMovieWrapper>
+        {id && (
+          <FormItem>
+            <Title>{ID_LABEL}</Title>
+            <MovieId>{id}</MovieId>
+          </FormItem>
+        )}
+        <FormItem>
+          <Title>{TITLE_LABEL}</Title>
+          {formik.errors.title && formik.touched.title && (
+            <Error>{formik.errors.title}</Error>
           )}
-          <FormItem>
-            <Title>{TITLE_LABEL}</Title>
-            {errors.title && touched.title && <Error>{errors.title}</Error>}
-            <input
-              type="text"
-              name="title"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.title}
-              placeholder={"Title here"}
-            />
-          </FormItem>
-          <FormItem>
-            <Title>{DATE_LABEL}</Title>
-            {errors.release_date && touched.release_date && (
-              <Error>{errors.release_date}</Error>
-            )}
-            <input
-              type="date"
-              name="release_date"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.release_date}
-            />
-          </FormItem>
-          <FormItem>
-            <Title>{URL_LABEL}</Title>
-            {errors.poster_path && touched.poster_path && (
-              <Error>{errors.poster_path}</Error>
-            )}
-            <input
-              type="text"
-              name="poster_path"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.poster_path}
-              placeholder={"Movie URL here"}
-            />
-          </FormItem>
-          <FormItem>
-            <Title>{GENRE_LABEL}</Title>
-            {errors.genres && touched.genres && <Error>{errors.genres}</Error>}
-            <select
-              name="genres"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.genres}
-              multiple
-            >
-              {genresData.map(({ value }) => (
-                <option value={value}>{value}</option>
-              ))}
-            </select>
-          </FormItem>
-          <FormItem>
-            <Title>{OVERVIEW_LABEL}</Title>
-            {errors.overview && touched.overview && (
-              <Error>{errors.overview}</Error>
-            )}
-            <input
-              type="text"
-              name="overview"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.overview}
-              placeholder={"Overview here"}
-            />
-          </FormItem>
-          <FormItem>
-            <Title>{RUNTIME_LABEL}</Title>
-            {errors.runtime && touched.runtime && (
-              <Error>{errors.runtime}</Error>
-            )}
-            <input
-              type="number"
-              name="runtime"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.runtime}
-              min={0}
-            />
-          </FormItem>
-          <ButtonGroup>
-            <Reset type="reset" onClick={handleReset}>
-              {resetButtonText}
-            </Reset>
-            <Submit type="submit" disabled={isSubmitting}>
-              {submitButtonText}
-            </Submit>
-          </ButtonGroup>
-        </FormMovieWrapper>
-      </form>
-    )}
-  </Formik>
-);
+          <input
+            type="text"
+            name="title"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.title}
+            placeholder={"Title here"}
+          />
+        </FormItem>
+        <FormItem>
+          <Title>{DATE_LABEL}</Title>
+          {formik.errors.release_date && formik.touched.release_date && (
+            <Error>{formik.errors.release_date}</Error>
+          )}
+          <input
+            type="date"
+            name="release_date"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.release_date}
+          />
+        </FormItem>
+        <FormItem>
+          <Title>{URL_LABEL}</Title>
+          {formik.errors.poster_path && formik.touched.poster_path && (
+            <Error>{formik.errors.poster_path}</Error>
+          )}
+          <input
+            type="text"
+            name="poster_path"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.poster_path}
+            placeholder={"Movie URL here"}
+          />
+        </FormItem>
+        <FormItem>
+          <Title>{GENRE_LABEL}</Title>
+          {formik.errors.genres && formik.touched.genres && (
+            <Error>{formik.errors.genres}</Error>
+          )}
+          <select
+            name="genres"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.genres}
+            multiple
+          >
+            {genresData.map(({ value }) => (
+              <option value={value}>{value}</option>
+            ))}
+          </select>
+        </FormItem>
+        <FormItem>
+          <Title>{OVERVIEW_LABEL}</Title>
+          {formik.errors.overview && formik.touched.overview && (
+            <Error>{formik.errors.overview}</Error>
+          )}
+          <input
+            type="text"
+            name="overview"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.overview}
+            placeholder={"Overview here"}
+          />
+        </FormItem>
+        <FormItem>
+          <Title>{RUNTIME_LABEL}</Title>
+          {formik.errors.runtime && formik.touched.runtime && (
+            <Error>{formik.errors.runtime}</Error>
+          )}
+          <input
+            type="number"
+            name="runtime"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.runtime}
+            min={0}
+          />
+        </FormItem>
+        <ButtonGroup>
+          <Reset type="reset" onClick={formik.handleReset}>
+            {resetButtonText}
+          </Reset>
+          <Submit type="submit" disabled={formik.isSubmitting}>
+            {submitButtonText}
+          </Submit>
+        </ButtonGroup>
+      </FormMovieWrapper>
+    </form>
+  );
+};
